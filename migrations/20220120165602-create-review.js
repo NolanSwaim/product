72cb88cb224c9c10d350e9a5dfe69b1b20fbb94b
nolanswaim <nolanswaim13@gmail.com>
2022-01-21 08:36:07 -0800

    initial commit

---
+++ migrations/20220120165602-create-review.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      customer_name: {
+        type: Sequelize.STRING
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      product_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reviews');
+  }
+};
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const productController = require("../controllers/productController.js");
+const reviewController = require('../controllers/reviewController.js');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.redirect('/products');
+});
+router.get('/products', productController.viewProducts);
+router.get('/products/profile/:id', productController.renderProfile);
+router.get('/products/edit/:id', productController.renderEditForm);
+router.post('/products/edit/:id', productController.updateProduct);
+router.get('/products/add', productController.renderAddForm);
+router.post('/products/add', productController.addProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
+
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm);
+router.post('/products/:productId/review/add', reviewController.addReview);
+module.exports = router;
